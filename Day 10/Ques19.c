94aefea0d093130e659d93748f2529694987e16e
Nirbhay Garg <nirbhaygarg11@gmail.com>
2025-08-30 20:45:41 +0530

    Main

---
+++ Day 10/Ques19.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int side1, side2, side3;
+    
+    printf("Enter the three sides of the triangle:\n");
+    printf("Side 1: ");
+    scanf("%d", &side1);
+    printf("Side 2: ");
+    scanf("%d", &side2);
+    printf("Side 3: ");
+    scanf("%d", &side3);
+    
+
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+        printf("Error: Sides must be positive numbers!\n");
+        return 1;
+    }
+    
+
+    if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+        printf("Error: These sides cannot form a triangle!\n");
+        return 1;
+    }
+    
+    
+    if (side1 == side2 && side2 == side3) {
+        printf("Equilateral\n");
+    }
+    else if (side1 == side2 || side2 == side3 || side1 == side3) {
+        printf("Isosceles\n");
+    }
+    else {
+        printf("Scalene\n");
+    }
+    
+    return 0;
+}
