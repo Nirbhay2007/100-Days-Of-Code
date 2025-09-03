7cbddec553d094c88905479c6e119bb4c1b0483e
Nirbhay Garg <nirbhaygarg11@gmail.com>
2025-09-03 14:26:03 +0530

    main

---
+++ Day 13/Ques.26.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ Day 14/Ques27.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sum += (2 * i - 1);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
