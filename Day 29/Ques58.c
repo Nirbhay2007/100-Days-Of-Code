701d17eeb2db11d50e7167c5880cbcc8eff25b2f
Nirbhay Garg <nirbhaygarg11@gmail.com>
2025-09-23 08:38:18 +0530

    Main

---
+++ Day 29/Ques58.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int min, max;
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        if (i == 0) {
+            min = max = x;
+        } else {
+            if (x < min) { min = x; }
+            if (x > max) { max = x; }
+        }
+    }
+    printf("min = %d \n", min);
+    printf("max = %d \n", max);
+    return 0;
+}
